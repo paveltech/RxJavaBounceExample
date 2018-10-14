5cca27e15002b6fb107a2c6a38e9ad9b61625b5a
Ariful Islam <ariful.pust@gmail.com>
2018-10-14 18:36:56 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dream71/rxjavabounceexample/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.dream71.rxjavabounceexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import com.jakewharton.rxbinding2.view.RxView;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtTapResult;
+    TextView txttapResultMax;
+    Button btnTapArea;
+
+    private Disposable disposable;
+    private int maxTaps = 0;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        txtTapResult = (TextView) findViewById(R.id.tap_result);
+        txttapResultMax = (TextView) findViewById(R.id.tap_result_max_count);
+        btnTapArea = (Button) findViewById(R.id.layout_tap_area);
+
+        RxView.clicks(btnTapArea)
+                .map(new Function<Object, Integer>() {
+                    @Override
+                    public Integer apply(Object o) throws Exception {
+                        return 1;
+                    }
+                })
+                .buffer(3 , TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(List<Integer> integers) {
+                        Log.e(TAG, "onNext: " + integers.size() + " taps received!");
+                        if (integers.size() > 0){
+                            maxTaps = integers.size() > maxTaps ? integers.size() : maxTaps;
+                            txtTapResult.setText(String.format("Received %d taps in 3 secs", integers.size()));
+                            txttapResultMax.setText(String.format("Maximum of %d taps received in this session", maxTaps));
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
